18f74db7fd3bec3fc6b808303505a74bf3fee3fe
Anthony Stanislaus <anthonystanislaus1@gmail.com>
2023-09-19 16:30:55 +0000

    adds s3 bucket resource with random name

--- main.tf
+++ main.tf
@@ -4,16 +4,28 @@ terraform {
       source  = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.17.0"
+    }
   }
 }
 
-provider "random" {
-  # Configuration options
+provider "random" {}
+
+provider "aws" {}
+
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
+resource "random_string" "bucket_name" {
+  length  = 32
+  special = false
+  upper   = false
 }
 
-resource "random_string" "bucket_name" {
-  length  = 16
-  special = false
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "my_bucket" {
+  # Bucket Naming Rules => https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
+  bucket = random_string.bucket_name.result
 }
 
 output "random_bucket_name_id" {
