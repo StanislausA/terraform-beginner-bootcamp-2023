1d89a651595f0241ddf1a44b1988d8de4f411e04
Anthony Stanislaus <anthonystanislaus1@gmail.com>
2023-09-26 20:24:43 +0000

    adds s3 module

---
+++ modules/terrahouse_aws/variables.tf
@@ -0,0 +1,19 @@
+variable "user_uuid" {
+  description = "UUID of the user"
+  type        = string
+
+  validation {
+    condition     = can(regex("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", var.user_uuid))
+    error_message = "The user_uuid value is not a valid UUID."
+  }
+}
+
+variable "bucket_name" {
+  description = "Name of bucket"
+  type        = string
+
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9.-]{3,63}$", var.bucket_name))
+    error_message = "Invalid AWS S3 bucket name"
+  }
+}
