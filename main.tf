823e7f278b5e0771f52605e2807dde7caa7e2cb3
Anthony Stanislaus <anthonystanislaus1@gmail.com>
2023-10-02 02:04:50 +0000

    adds cache invalidation

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {}
 
 module "terrahouse_aws" {
-  source      = "./modules/terrahouse_aws"
-  user_uuid   = var.user_uuid
-  bucket_name = var.bucket_name
-  index_file_path  = var.index_file_path
-  error_file_path  = var.error_file_path
+  source          = "./modules/terrahouse_aws"
+  user_uuid       = var.user_uuid
+  bucket_name     = var.bucket_name
+  index_file_path = var.index_file_path
+  error_file_path = var.error_file_path
   content_version = var.content_version
 }
