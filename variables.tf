addf8e56b63515bb3010ac8a264199a240342c3c
Anthony Stanislaus <anthonystanislaus1@gmail.com>
2023-10-06 20:04:55 +0000

    enables deployment to Terratowns

--- variables.tf
+++ variables.tf
@@ -7,3 +7,9 @@ variable "index_file_path" {}
 variable "error_file_path" {}
 
 variable "content_version" {}
+
+variable "terratowns_url" {}
+
+variable "terratowns_access_token" {}
+
+variable "town_name" {}
